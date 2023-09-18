55adffd9ec25285030e0bf59636b43983f68cb38
Olenka Vavrychuk <40758661+Olena09@users.noreply.github.com>
2023-09-18 20:20:43 +0300

    update1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector(".gallery")
+
+function createGalleryItem(item) {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+        />
+      </a>
+    </li>`;
+}
+
+const galleryMarkup = galleryItems.map(createGalleryItem).join('');
+gallery.innerHTML = galleryMarkup;
+
+gallery.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (e.target.tagName === 'IMG') {
+      const imageUrl = e.target.dataset.source;
+
+      const lightbox = basicLightbox.create(`
+      <img src="${imageUrl}" width="800" height="600">`);
+
+      lightbox.show();
+    }
+});
+
 console.log(galleryItems);
